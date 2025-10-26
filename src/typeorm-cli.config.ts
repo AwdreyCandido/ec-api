d2b9430df161adfdb6fbb2790f9828baa0f31fd5
AwdreyCandido <awdrey.dev@gmail.com>
2025-10-26 06:01:50 -0300

    prod: preparing for deploy

---
+++ src/typeorm-cli.config.ts
@@ -0,0 +1,32 @@
+import { DataSource } from 'typeorm';
+import { User } from './app/users/entities/user.entity';
+import { Product } from './app/products/entities/product.entity';
+import { Store } from './app/stores/entities/store.entity';
+import { Review } from './app/reviews/entities/review.entity';
+import { Order } from './app/orders/entities/order.entity';
+import { OrderGroup } from './app/orders/entities/order-group.entity';
+import { ShoppingCart } from 'src/app/carts/entities/shopping-cart.entity';
+import { CartItem } from 'src/app/carts/entities/cart-item.entity';
+
+const AppDataSource = new DataSource({
+  type: 'mysql',
+  host: 'sql.freedb.tech',
+  port: 3306,
+  username: 'freedb_ecroot',
+  password: 'U76RTuf*TDB@YgA',
+  database: 'freedb_ecommerce-test',
+  entities: [
+    User,
+    Product,
+    Store,
+    Review,
+    Order,
+    OrderGroup,
+    ShoppingCart,
+    CartItem,
+  ],
+  migrations: ['src/migrations/*.js'],
+  synchronize: false,
+});
+
+export default AppDataSource;
